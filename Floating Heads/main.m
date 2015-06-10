a8e60e48bd1e344acd09491f1ad4aeb3e6f94541
iantsai <iantsai86@gmail.com>
2015-06-10 11:00:04 -0700

    Initial Commit

---
+++ Floating Heads/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Floating Heads
+//
+//  Created by Ian Tsai on 2015-06-10.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
